39d55be7b791d0f2f65b1b47082d4b8bd5e562e9
BuildTools <unconfigured@null.spigotmc.org>
2019-04-26 18:11:11 +0800

    Fix dummy integer.

--- src/main/java/me/i234/gangamlorder/learning1/listener/BucketListener.java
+++ src/main/java/me/i234/gangamlorder/learning1/listener/BucketListener.java
@@ -44,7 +44,6 @@ public class BucketListener implements Listener {
         allowed.add(Material.VOID_AIR);
 
         int currentY = location.getBlockY();
-        int test;
 
 
         Generator task = new Generator(location, Material.valueOf(nbtItem.getString("material")), allowed, GenerateType.valueOf(nbtItem.getString("generateType")));
