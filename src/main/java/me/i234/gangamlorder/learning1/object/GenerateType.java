e39605caf7ffc5bc80529f28427f294312e29fde
BuildTools <unconfigured@null.spigotmc.org>
2019-04-26 18:07:34 +0800

    Add new generator and begin inclusion of NBT frameworks.

---
+++ src/main/java/me/i234/gangamlorder/learning1/object/GenerateType.java
@@ -0,0 +1,8 @@
+package me.i234.gangamlorder.learning1.object;
+
+public enum GenerateType {
+    /**
+     * UP denotes upwards generation, DOWN denotes downwards veneration, SIDE denotes sideways generation.
+     */
+    UP, DOWN
+}
--- src/main/java/me/i234/gangamlorder/learning1/utils/Generator.java
+++ src/main/java/me/i234/gangamlorder/learning1/utils/Generator.java
@@ -1,6 +1,6 @@
 package me.i234.gangamlorder.learning1.utils;
 
-import me.i234.gangamlorder.learning1.object.TemporaryObject;
+import me.i234.gangamlorder.learning1.object.GenerateType;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.World;
@@ -9,7 +9,6 @@ import org.bukkit.scheduler.BukkitRunnable;
 
 import java.util.HashSet;
 import java.util.Set;
-import java.util.UUID;
 
 public class Generator extends BukkitRunnable {
 
@@ -17,27 +16,22 @@ public class Generator extends BukkitRunnable {
     private Material toSet;
     private Set<Material> allowed = new HashSet<>();
     private int currentY;
-    private UUID uuid;
+    private GenerateType generateType;
 
     private Generator() {
     }
 
-    public Generator(UUID uuid, Location original, Material toSet, Set<Material> allowed) {
+    public Generator(Location original, Material toSet, Set<Material> allowed, GenerateType generateType) {
         this();
-
-        if (TemporaryObject.getY(uuid) == -1) {
-            TemporaryObject.registerTask(uuid, original.getBlockY());
-        }
-
         this.allowed = allowed;
         this.original = original;
         this.toSet = toSet;
-        this.uuid = uuid;
-        this.currentY = TemporaryObject.getY(uuid);
+        this.generateType = generateType;
 
-        if (currentY < 0 || currentY > 255) {
-            throw new IllegalStateException("Invalid Y limit.");
+        if (original.getWorld() == null) {
+            throw new IllegalStateException("Null world!");
         }
+
     }
 
     @Override
@@ -49,31 +43,43 @@ public class Generator extends BukkitRunnable {
         Location back = original.add(0, -1, -1);
         World world = left.getWorld();
 
+        int y = 0;
+
+        switch (generateType) {
+            case UP:
+                y = currentY + 1;
+                break;
+            case DOWN:
+                y = -currentY - 1;
+                break;
+        }
+
         if (allowed.contains(left.getBlock().getType())) {
-            Block block = world.getBlockAt(left.add(0, -currentY + 1, 0));
+            Block block = world.getBlockAt(left.add(0, y, 0));
             if (allowed.contains(block.getType())) {
-                block.setType(Material.COBBLESTONE);
+                block.setType(toSet);
             }
         } else if (allowed.contains(right.getBlock().getType())) {
-            Block block = world.getBlockAt(right.add(0, -currentY + 1, 0));
+            Block block = world.getBlockAt(right.add(0, y, 0));
             if (allowed.contains(block.getType())) {
-                block.setType(Material.COBBLESTONE);
+                block.setType(toSet);
             }
         } else if (allowed.contains(front.getBlock().getType())) {
-            Block block = world.getBlockAt(front.add(0, -currentY + 1, 0));
+            Block block = world.getBlockAt(front.add(0, y, 0));
             if (allowed.contains(block.getType())) {
-                block.setType(Material.COBBLESTONE);
+                block.setType(toSet);
             }
         } else if (allowed.contains(back.getBlock().getType())) {
-            Block block = world.getBlockAt(back.add(0, -currentY + 1, 0));
+            Block block = world.getBlockAt(back.add(0, y, 0));
             if (allowed.contains(block.getType())) {
-                block.setType(Material.COBBLESTONE);
+                block.setType(toSet);
             }
         }
-        currentY = currentY - 1;
+
         if (!allowed.contains(world.getBlockAt(original.add(0, currentY, 0)).getType())) {
             this.cancel();
         }
-        TemporaryObject.updateY(this.uuid, currentY);
+
+        currentY = (generateType == GenerateType.UP) ? currentY + 1 : currentY - 1;
     }
 }
